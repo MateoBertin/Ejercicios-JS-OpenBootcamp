525399ee43e9c9a935dc075684515bc56e641ef3
MateoBertin <115265488+MateoBertin@users.noreply.github.com>
2023-04-12 18:34:43 -0300

    Add files via upload

---
+++ ejercicio-2.js
@@ -0,0 +1,10 @@
+
+
+const listaYo = ["Mateo", 16, true, Date(2006, 5, 27), 
+libro = { titulo: "Atomic Habits", 
+autor: "James Clear",
+fecha: new Date("16 octubre 2018"),
+url: "https://www.amazon.com/Atomic-Habits-Proven-Build-Break/dp/0735211299"}
+]
+
+console.log(listaYo[4].fecha)
