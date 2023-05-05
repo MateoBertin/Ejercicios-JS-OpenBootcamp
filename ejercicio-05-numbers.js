161a3ac654d662a65e16e15a3b5b14ad62a8bf53
MateoBertin <115265488+MateoBertin@users.noreply.github.com>
2023-05-05 18:39:47 -0300

    Delete ejercicio-05-numbers.js

--- ejercicio-05-numbers.js
+++
@@ -1,9 +0,0 @@
-let alt_cm = 183;
-let alt_m = 1.83;
-let peso = 50.5
-let alt_arriba = alt_m.toFixed()
-console.log(alt_arriba);
-let peso_abajo = Math.floor(peso)
-console.log(peso_abajo);
-let max_val = Number.MAX_VALUE + 1 == Number.MIN_VALUE
-console.log(max_val);
